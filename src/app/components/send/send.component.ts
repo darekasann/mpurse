9fa29857ff4467908c4c0d13c76fc4be90c9c92c
darekasann <39562087+darekasann@users.noreply.github.com>
2019-06-24 20:53:48 +0900

    Update send.component.ts

--- src/app/components/send/send.component.ts
+++ src/app/components/send/send.component.ts
@@ -64,7 +64,7 @@ export class SendComponent implements OnInit {
   }, {validators: this.memoValidator});
 
   assetControl = new FormControl('MONA', [Validators.required]);
-  feeControl = new FormControl(11, [Validators.required, Validators.min(0)]);
+  feeControl = new FormControl(11, [Validators.required, Validators.min(1)]);
 
   sendForm = new FormGroup({
     from: this.fromControl,
